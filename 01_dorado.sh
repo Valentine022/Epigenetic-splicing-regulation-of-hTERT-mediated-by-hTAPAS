478391825c3d13503f94c7fca31cedb60113143b
Valentine <46489777+Valentine022@users.noreply.github.com>
2026-09-16 19:59:39 +0100

    Update 01_dorado.sh

--- 01_dorado.sh
+++ 01_dorado.sh
@@ -13,8 +13,8 @@ module load samtools/1.16.1
 module load guppy
 module load pigz
 
-POD5_DIR="/media/data_01/shared/SEQUENCING_NANOPORE/178a_LE_hTERT_cd4t/pod5"
-OUTPUT_DIR="/media/data_01/vapatter/PROJECTS/15-LARS/178a_LE_hTERT_cd4t/T2T"
+POD5_DIR="/media/data_01/shared/SEQUENCING_NANOPORE/125_NDLE_hTERT_CRISPR_VA13/pod5"
+OUTPUT_DIR="/media/data_01/vapatter/PROJECTS/15-LARS/125_NDLE_hTERT_CRISPR_VA13/T2T"
 REF="/media/data_01/shared/GENOMES/homo_sapiens/T2T-CHM13v2.0/genome/T2T-CHM13v2.0.fasta"
 
 dorado basecaller \
@@ -22,4 +22,4 @@ dorado basecaller \
     --recursive \
     --device cuda:0 \
     sup,5mC_5hmC \
-    $POD5_DIR > $OUTPUT_DIR/178_T2T-CHM13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.bam
+    $POD5_DIR > $OUTPUT_DIR/125_VA13_CRISPR_T2T-CHM13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.bam
