5e0d2ee6d0a6496d2663813db1595209909579d7
Valentine <46489777+Valentine022@users.noreply.github.com>
2026-09-16 20:01:35 +0100

    Update 03_samtools_fastq.sh

--- 03_samtools_fastq.sh
+++ 03_samtools_fastq.sh
@@ -14,16 +14,10 @@ module load dorado/0.8.0
 module load samtools/1.20
 module load pigz/2.8
 
-OUTPUT_DIR="/media/data_01/vapatter/PROJECTS/15-LARS/137_LE_hTERT_CRISPR_HEK_Aza_cutsitetest/T2T"
+OUTPUT_DIR="/media/data_01/vapatter/PROJECTS/15-LARS/125_NDLE_hTERT_CRISPR_VA13/T2T"
 
 cd $OUTPUT_DIR
 
-/usr/bin/time -v samtools fastq -@ 25 HEK_Aza_cutsitetest_CRISPR_T2T-CHM13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.bam.sorted.bam > \
-HEK_Aza_cutsitetest_CRISPR_T2T-CHM13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.fastq
-pigz HEK_Aza_cutsitetest_CRISPR_T2T-CHM13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.fastq
-
-#/usr/bin/time -v samtools fastq -@ 25 barcode24_CRISPR_chm13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.bam.sorted.bam > barcode24_CRISPR_chm13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.fastq
-#pigz barcode24_CRISPR_chm13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.fastq
-
-#/usr/bin/time -v samtools fastq -@ 25 barcode22_CRISPR_chm13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.bam.sorted.bam > barcode22_CRISPR_chm13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.fastq
-#pigz barcode22_CRISPR_chm13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.fastq
+/usr/bin/time -v samtools fastq -@ 25 125_VA13_CRISPR_T2T-CHM13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.bam.sorted.bam.sorted.bam > \
+125_VA13_CRISPR_T2T-CHM13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.bam.sorted.bam.fastq
+pigz 125_VA13_CRISPR_T2T-CHM13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.bam.sorted.bam.fastq
