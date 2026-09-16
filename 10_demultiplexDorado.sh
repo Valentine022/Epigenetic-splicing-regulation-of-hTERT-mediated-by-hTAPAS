daa9faddb70496908eacafbb78a86cc321a1f39b
Valentine <46489777+Valentine022@users.noreply.github.com>
2026-09-16 20:03:57 +0100

    Update 10_demultiplexDorado.sh

--- 10_demultiplexDorado.sh
+++ 10_demultiplexDorado.sh
@@ -13,14 +13,14 @@ module load samtools/1.16.1
 module load guppy
 module load pigz
 
-OUTPUT_DIR="/media/data_01/vapatter/PROJECTS/15-LARS/178a_LE_hTERT_cd4t/T2T"
+OUTPUT_DIR="/media/data_01/vapatter/PROJECTS/15-LARS/125_NDLE_hTERT_CRISPR_VA13/T2T"
 REF="/media/data_01/shared/GENOMES/homo_sapiens/T2T-CHM13v2.0/genome/T2T-CHM13v2.0.fasta"
 
 dorado demux --no-trim --sort-bam --kit-name EXP-NBD114 --output-dir $OUTPUT_DIR/DORADO_DEMUX \
-$OUTPUT_DIR/178_T2T-CHM13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.bam
+$OUTPUT_DIR/125_VA13_CRISPR_T2T-CHM13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.bam.sorted.bam
 
 dorado demux --no-trim --sort-bam --kit-name EXP-NBD104 --output-dir $OUTPUT_DIR/DORADO_DEMUX \
-$OUTPUT_DIR/178_T2T-CHM13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.bam
+$OUTPUT_DIR/125_VA13_CRISPR_T2T-CHM13v2.0.dna_r10.4.1_e8.2_400bps_sup@v4.3.0.bam.sorted.bam
 
 
 sbatch 11_dorado_align.sh
